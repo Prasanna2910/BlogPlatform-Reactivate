daa5d841dca90e8f02a63aa99d5380e202020cfd
Sahanashre-V <sahanashre.v@gmail.com>
2025-03-13 15:15:12 +0530

    Done

--- vite-project/src/App.jsx
+++ vite-project/src/App.jsx
@@ -1,12 +1,22 @@
 import { useState } from 'react'
 import Navbar from "./Component/Navbar"
-
+import Landing from './Component/Landing'
+import Main from "./Component/Main"
+import Blogs from "./Component/AllBlogs"
+import { BrowserRouter, Route, Routes } from 'react-router-dom'
 
 function App() {
 
   return (
     <>
-    <Navbar />
+    <BrowserRouter>
+    <Routes>
+      <Route path="/" element={<Landing/>}/>
+      <Route path="/nav" element={<Navbar/>}/>
+      <Route path="/post" element={<Main />}/>
+      <Route path="/blogs" element={<Blogs/>}/>
+    </Routes>
+    </BrowserRouter>
     </>
   )
 }
---
+++ vite-project/src/Component/Main.jsx
@@ -0,0 +1,74 @@
+import { useNavigate } from "react-router-dom";
+import Navbar from "./Navbar";
+import React, { useState, useEffect } from "react";
+
+const Main = () => {
+  const [posts, setPosts] = useState([]);
+  const [title, setTitle] = useState("");
+  const [content, setContent] = useState("");
+  const navigate = useNavigate();
+
+  useEffect(() => {
+    const savedPosts = JSON.parse(localStorage.getItem("posts")) || [];
+    setPosts(savedPosts);
+  }, []);
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    const newPost = { title, content };
+    const updatedPosts = [...posts, newPost];
+    setPosts(updatedPosts);
+    localStorage.setItem("posts", JSON.stringify(updatedPosts));
+    setTitle("");
+    setContent("");
+    
+    // Navigate after posting
+    navigate("/blogs");
+  };
+
+  return (
+    <>
+      <Navbar />
+      <div className="max-w-4xl mx-auto p-6">
+        <form onSubmit={handleSubmit} className="mb-6 ">
+          <input
+            type="text"
+            placeholder="Title"
+            value={title}
+            onChange={(e) => setTitle(e.target.value)}
+            className="w-full h-12 border p-3 mb-2 rounded text-lg"
+            required
+          />
+
+          <textarea
+            placeholder="Content"
+            value={content}
+            onChange={(e) => setContent(e.target.value)}
+            className="w-full h-96 p-2 mb-2 border rounded"
+            required
+          ></textarea>
+
+          <div className=" flex justify-center"><div className=" flex justify-between w-96 items-center p-2">
+            <button
+              type="submit"
+              className="bg-[#3D3D3D] text-white px-4 py-2 rounded"
+            >
+              Post
+            </button>
+
+            <button
+              type="button"
+              className="bg-[#3D3D3D] text-white px-4 py-2 rounded"
+              onClick={() => navigate("/blogs")}
+            >
+              View all blogs
+            </button>
+          </div>
+          </div>
+        </form>
+      </div>
+    </>
+  );
+};
+
+export default Main;
